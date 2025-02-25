b43ca9e4267f6c4c2958f2a772cbeba516272e46
Jägermeist <phillenth04@outlook.com>
2025-02-25 18:06:24 +0100

    added server

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a ChatRoom in the browser
--- server.py
+++ server.py
@@ -1,2 +1,24 @@
-import flask
-import flask_socketio
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+clients = {}
+
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+@socketio.on("connect")
+def handle_connect():
+    print(f"Client: {request.sid} connected!")
+    clients[request.sid] = {"messages": []}
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
